0f82aef06ed69b950202de020de676a497927075
Jhonatan Agustin Cueva Mendoza <80427530+Jhonatan-Polar@users.noreply.github.com>
2024-06-30 10:52:56 -0500

    Induccion a Junit5

---
+++ junit-5-basics-course-master/src/main/java/io/javabrains/utils/Otro.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package io.javabrains.utils;
+
+/**
+ *
+ * @author user
+ */
+public class Otro {
+    
+    public int add2(int a, int b) {
+		return a + b;
+	}
+}
---
+++ junit-5-basics-course-master/src/test/java/io/javabrains/utils/OtroTest.java
@@ -0,0 +1,15 @@
+package io.javabrains.utils;
+
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class OtroTest {
+    
+    @Test
+    void test(){
+        //fail("Holi");
+        System.out.println("Holi");
+    }
+}
